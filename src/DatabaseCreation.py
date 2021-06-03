bd02e5102c6825be5095238c7102781b66bff5d3
marait123 <max.ibrahim43@hotmail.com>
2021-06-03 21:51:22 +0200

    sent meta data

--- src/DatabaseCreation.py
+++ src/DatabaseCreation.py
@@ -3,13 +3,27 @@ import json
 import pandas as pd
 import math
 
+clients = [
+    "tcp://127.0.0.1:8001",  # the master
+    "tcp://127.0.0.1:8002",  # tablet server 1
+    "tcp://127.0.0.1:8003",  # tablet server 2
+    "tcp://127.0.0.1:8004",  # client server 1
+    "tcp://127.0.0.1:8004",  # client server 2
+]
+# read csv from excel and save it in array of json
+
+
 def importData(csv_path):
     data = pd.read_csv(csv_path)
     print("dataRead")
     payload = json.loads(data.to_json(orient='records'))
     print(payload[0])
     return payload
-def database ():
+# Create Database and collection
+# if database exits then no need to create it
+
+
+def database():
     try:
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         dblist = myclient.list_database_names()
@@ -23,36 +37,44 @@ def database ():
             print("The database created.")
             data = importData("../database/googleplaystore.csv")
             mycol = mydb["apps"]
-            #mycol.delete_many()
+            # mycol.delete_many()
             mycol.insert_many(data)
             return mycol
-    except: 
+    except:
         print("Can't connect to database")
 
+
 mycol = database()
+# Sort Table by Category to use it to divide it into tablets
 BigTable = mycol.find().sort("Category")
+# Get Number of Categories in table
 Categories = mycol.distinct("Category")
-NoOfCategories = len(Categories)
-Tablets = [[],[],[],[]]
-NoOfCategoriesPerTablet = math.floor(NoOfCategories/4)
-NoInCurrentTablet = 0
-TabletNo = 0
-TabletPerMachine = 2
+no_of_categories = len(Categories)
+# Create tablets
+# map categories to tablets
+# map tablets to machines
+Tablets = [[], [], [], []]
+no_of_categories_Per_Tablet = math.floor(no_of_categories/4)
+no_in_currentTablet = 0
+tablet_no = 0
+tablet_per_machine = 2
 Machine = 2
-dicTabletinMachine = {}
-dicCategoryinTablet = {}
-for i in range(NoOfCategories):
-    if NoInCurrentTablet == NoOfCategoriesPerTablet and TabletNo < 3:
-        dicTabletinMachine[TabletNo] = Machine
-        NoInCurrentTablet = 0
-        TabletNo += 1
-        if TabletNo == TabletPerMachine:
+dic_tablet_in_machine = {}
+dic_category_in_tablet = {}
+for i in range(no_of_categories):
+    if no_in_currentTablet == no_of_categories_Per_Tablet and tablet_no < 3:
+        dic_tablet_in_machine[tablet_no] = Machine
+        no_in_currentTablet = 0
+        tablet_no += 1
+        if tablet_no == tablet_per_machine:
             Machine += 1
-    myquery = {"Category":Categories[i]}
+    myquery = {"Category": Categories[i]}
     category = list(mycol.find(myquery))
-    Tablets[TabletNo].append(category)
-    dicCategoryinTablet[Categories[i]] = TabletNo
-    NoInCurrentTablet += 1
-dicTabletinMachine[TabletNo] = Machine
-print(dicTabletinMachine)
-print(dicCategoryinTablet)
+    Tablets[tablet_no].append(category)
+    dic_category_in_tablet[Categories[i]] = tablet_no
+    no_in_currentTablet += 1
+dic_tablet_in_machine[tablet_no] = Machine
+
+print(dic_tablet_in_machine)
+print(dic_category_in_tablet)
+print()
--- src/Master.py
+++ src/Master.py
@@ -1,12 +1,44 @@
 import zmq
 from zmq.sugar import socket
+from zmq.sugar.constants import NOBLOCK
+from .DatabaseCreation import Tablets, dic_category_in_tablet
 context = zmq.Context()
-
 socket = context.socket(zmq.REP)
 socket.bind("tcp://127.0.0.1:8001")
 
+arr_ip_tablet = [
+
+]
+# 0
+# 1
+# 2
+# 3
 while True:
-    msg = socket.recv()
+    msg = socket.recv(flags=zmq.NOBLOCK)
+    try:
+        # check for a message, this will not block
+        message = socket.recv(flags=zmq.NOBLOCK)
+        if msg == "alive-client":
+            # meta data is sent to the client
+            socket.send_json(arr_ip_tablet)
+            socket.send_json(dic_category_in_tablet)
+        elif msg == "alive-server":
+            ip = socket.recv()
+            arr_ip_tablet.append({
+                'ip': ip,
+                'tablets': {
+                    len(arr_ip_tablet),
+                    len(arr_ip_tablet)+1
+                }
+            })
+            # send the 2 tablets
+            socket.send_json(Tablets[len(arr_ip_tablet)-1])
+            socket.send_json(Tablets[len(arr_ip_tablet)])
+            # send to that ip
+
+    except zmq.Again as e:
+        print("No message received yet")
+
     print("From Client :", msg)
     smsg = input('enter your message : ')
     socket.send_string(smsg)
