e08ffea0a746d1c6ea0bae039a2bd343f6ca6d76
omar9984 <omartarek9984@gmail.com>
2021-06-04 01:00:42 +0200

    Making the sockets between Server and Master

--- src/Master.py
+++ src/Master.py
@@ -1,51 +1,94 @@
 import zmq
 from zmq.sugar import socket
 from zmq.sugar.constants import NOBLOCK
-from .DatabaseCreation import Tablets, dic_category_in_tablet
+import time
+from DatabaseCreation import Tablets, dic_category_in_tablet, ServerMasterPort,MasterServerPort, localhost
+import threading
+
 context = zmq.Context()
-socket = context.socket(zmq.REP)
-socket.bind("tcp://127.0.0.1:8001")
+# to make polling
+socketM_S = context.socket(zmq.REQ)
+socketM_S.connect(f"{localhost}:{MasterServerPort}")
 
-arr_ip_tablet = [
+# to used to notify the master of queries
+socketS_M = context.socket(zmq.REP)
+socketS_M.bind(f"{localhost}:{ServerMasterPort}")
 
-]
+# arr_ip_tablet = [
+
+# ]
 # 0
 # 1
 # 2
 # 3
+
+# ================================ Initialization =======================================
+# if msg == "alive-client":
+#             # meta data is sent to the client
+#             socket.send_json(arr_ip_tablet)
+#             socket.send_json(dic_category_in_tablet)
+#         elif msg == "alive-server":
+#             ip = socket.recv()
+#             arr_ip_tablet.append({
+#                 'ip': ip,
+#                 'tablets': {
+#                     len(arr_ip_tablet),
+#                     len(arr_ip_tablet)+1
+#                 }
+#             })
+#             # send the 2 tablets
+#             socket.send_json(Tablets[len(arr_ip_tablet)-1])
+#             socket.send_json(Tablets[len(arr_ip_tablet)])
+# =========================================================================================
+# rows_limit = 50
+# def Load_balancing(rows_number):
+#     if(rows_number > rows_limit):
+#         # load balancing
+#     # meta data update
+#     arr_ip_tablet.append({
+#         'ip': ip,
+#         'tablets': {
+#             len(arr_ip_tablet),
+#             len(arr_ip_tablet)+1
+#         }
+def Polling():
+    socketM_S.send_string("Ezayak bs2al fek msh zayak!")
+    print("he replied ",socketM_S.recv().decode('utf-8'))
+
+    
+Start_interval = time.time()
 while True:
-    msg = socket.recv(flags=zmq.NOBLOCK)
     try:
-        # check for a message, this will not block
-        message = socket.recv(flags=zmq.NOBLOCK)
-        if msg == "alive-client":
-            # meta data is sent to the client
-            socket.send_json(arr_ip_tablet)
-            socket.send_json(dic_category_in_tablet)
-        elif msg == "alive-server":
-            ip = socket.recv()
-            arr_ip_tablet.append({
-                'ip': ip,
-                'tablets': {
-                    len(arr_ip_tablet),
-                    len(arr_ip_tablet)+1
-                }
-            })
-            # send the 2 tablets
-            socket.send_json(Tablets[len(arr_ip_tablet)-1])
-            socket.send_json(Tablets[len(arr_ip_tablet)])
-            # send to that ip
-        elif msg == "data":
-            # meta data change
-            operation = socket.recv("")
+        
+        msg = socketS_M.recv(flags=NOBLOCK)
+        # Data update  (set or delete cell)
+        # todo: update main db
+        if   msg.decode('utf-8') == "update":
+            # Table change
+            print("Data update has occurred")
+            # operation = socketS_M.recv("")
+            socketS_M.send_string("done update")
             pass
-        for server in arr_ip_tablet:
-            ip = server['ip']
+        
+        # Structure update (add row or delete row)
+        # todo: check for balancing & and balance if needed
+        elif msg.decode('utf-8') == "struc":
+            print("Structure update has occurred")
+            socketS_M.send_string("done structure")
+            pass
+        else:
+            socketS_M.send_string("lol")
+        # todo: check for active tablet servers every interval (Polling)
+        # for server in arr_ip_tablet:
+        #     ip = server['ip']
+
+        if time.time() - Start_interval > 20:
+            Polling()
+            Start_interval = time.time()
 
     except zmq.Again as e:
-        print("No message received yet")
+        print("2")
+        #time.sleep(1)
+        #print("No message received yet")
 
-    print("From Client :", msg)
-    smsg = input('enter your message : ')
-    socket.send_string(smsg)
-    print("")
+    
--- src/Server.py
+++ src/Server.py
@@ -1,13 +1,36 @@
 import zmq
 from zmq.sugar import socket
+from zmq.sugar.constants import NOBLOCK, NULL
+
+localhost = "tcp://127.0.0.1" 
+ServerMasterPort = 8001  # Server - Master (req-rep) used to notify the master of queries
+MasterServerPort = 8002  # Master - server  (req-rep) used to make Polling
+
+
 context = zmq.Context()
+# to make polling
+socketM_S = context.socket(zmq.REP)
+socketM_S.bind(f"{localhost}:{MasterServerPort}")
 
-socket = context.socket(zmq.REP)
-socket.bind("tcp://127.0.0.1:8001")
+# to used to notify the master of queries
+socketS_M = context.socket(zmq.REQ)
+socketS_M.connect(f"{localhost}:{ServerMasterPort}")
+lol = True
 
+# Initialization 
+# Initialize the tablet data
+#================================================
 while True:
-    msg = socket.recv()
-    print("From Client :", msg)
-    smsg = input('enter your message : ')
-    socket.send_string(smsg)
-    print("")
+    lol = input('enter your message : ')
+    if lol == '1':
+        socketS_M.send_string("update",flags=NOBLOCK)
+    else:
+        socketS_M.send_string("struc",flags=NOBLOCK)
+    try:
+        msg = socketM_S.recv(flags=NOBLOCK)
+        if msg is not None:
+            msg = msg.decode('utf-8')
+            socketM_S.send_string("!* gg ez wp jng diff *!")
+            #print("From Client :", msg)
+    except zmq.Again as e:
+        print()
