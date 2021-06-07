11c8175123d7c6b028ae593e132e26a67c605e22
mabobakr <m.abobakr10@yahoo.com>
2021-06-07 13:04:24 +0200

    comment some unnecessary logging to console

--- node/queries.js
+++ node/queries.js
@@ -2,7 +2,7 @@ const _ = require('lodash')
 
 module.exports.set = async function set(Model, query) {
 	obj = _.omit(query, ['operation', 'Category'])
-	console.log(obj)
+	// console.log(obj)
 	let app = await Model.findOneAndUpdate({ Category: query.Category, App: query.App }, obj)
 	
 	app = await Model.find({App:query.App})
@@ -14,7 +14,7 @@ module.exports.deleteCells = async function deleteCells(Model, query) {
 	Object.keys(obj).forEach(o => {
         obj[o] = 1
     })
-	console.log(obj)
+	// console.log(obj)
 	let app = await Model.findOneAndUpdate({ Category: query.Category, App: query.App }, { $unset: obj})
 	
 	app = await Model.find({App:query.App})
@@ -33,7 +33,7 @@ module.exports.read = async function read(Model, query) {
 
 module.exports.addRow = async function addRow(Model, query) {
 	obj = _.omit(query, ['operation'])
-	console.log(obj)
+	// console.log(obj)
 	let app = await Model.create(obj)
     console.log(app)
 }
